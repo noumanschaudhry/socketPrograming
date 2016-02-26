95251870acf18c92c4e280047fa26cee1f9e09d9
noumanschaudhry <noumanschaudhry@gmail.com>
2016-02-26 12:55:00 +0500

    Create server.java

---
+++ server.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nchaudhry.bscs13seec
+ */
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+  public final static int SOCKET_PORT = 13267;  // you may change this
+  public final static String FILE_TO_SEND = "c:/temp/source.pdf";  // you may change this
+
+  public final static int FILE_SIZE = 6022386; 
+  
+  public static void main (String [] args ) throws IOException {
+      notes n = new notes();
+    FileInputStream fis = null;
+    BufferedInputStream bis = null;
+    OutputStream os = null;
+    ServerSocket servsock = null;
+    Socket sock = null;
+    try {
+      servsock = new ServerSocket(SOCKET_PORT);
+      while (true) {
+        System.out.println("Waiting...");
+        try {
+          sock = servsock.accept();
+          System.out.println("Accepted connection : " + sock);
+          // send file
+          notes nomi = new notes();
+          byte [] mybytearray  = new byte [(int)nomi.getinput];
+          fis = new FileInputStream(myFile);
+          bis = new BufferedInputStream(fis);
+          bis.read(mybytearray,0,mybytearray.length);
+          os = sock.getOutputStream();
+          System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+          os.write(mybytearray,0,mybytearray.length);
+          os.flush();
+          System.out.println("Done.");
+        }
+        finally {
+          if (bis != null) bis.close();
+          if (os != null) os.close();
+          if (sock!=null) sock.close();
+        }
+      }
+    }
+    finally {
+      if (servsock != null) servsock.close();
+    }
+  }
+}
+
